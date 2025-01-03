8ac04014195640ecc531764ea18a525792ac9caa
Yoshani Gamage <157863413+yrgamage@users.noreply.github.com>
2025-01-03 13:25:05 +0530

    Implement CRUD operations for order form

--- src/edu/ijse/mvc/dto/OrderDetaildto.java
+++ src/edu/ijse/mvc/dto/OrderDetaildto.java
@@ -9,5 +9,60 @@ package edu.ijse.mvc.dto;
  * @author User
  */
 public class OrderDetaildto {
+     private String orderId;
+    private String itemCode;
+    private int qty;
+    private double discount;
+
+    public OrderDetaildto() {
+    }
+
+    
+    public OrderDetaildto(String orderId, String itemCode, int qty, double discount) {
+        this.orderId = orderId;
+        this.itemCode = itemCode;
+        this.qty = qty;
+        this.discount = discount;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetaildto{" + "orderId=" + orderId + ", itemCode=" + itemCode + ", qty=" + qty + ", discount=" + discount + '}';
+    }
+    
+    
+    
     
 }
